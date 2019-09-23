e16b9e2d487cffd0b4772d98984c5fb4bcb19c83
Prof-Greipl <dieter.greipl@haw-landshut.de>
2019-09-23 12:49:04 +0200

    Removed file

--- z_getIML.bat
+++
@@ -1,4 +0,0 @@
-cd C:\A_WIF640-SEMINAR-MACHINE-LEARNING\
-rmdir /s /q colab_notebooks
-mkdir colab_notebooks
-xcopy "C:\Users\diete\Google Drive\Colab Notebooks\WIF-640\IML*" C:\A_WIF640-SEMINAR-MACHINE-LEARNING\colab_notebooks /s /e
